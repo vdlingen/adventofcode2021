5eee7ef653331605c4688145ac77a17c68fad57b
Ronald van der Lingen <ronald@vdlingen.nl>
2021-12-03 07:11:47 +0100

    Day 3 - cleanup

--- src/main/kotlin/day03.kt
+++ src/main/kotlin/day03.kt
@@ -4,47 +4,45 @@ val input = util.readInput("day03.txt")
     .split("\n")
 
 fun part1(): Int {
-    val gamma = input[0].indices.map { index ->
-        val counts = input.map { it[index] }.groupBy { it }.mapValues { it.value.size }
+    val gamma = input[0].indices
+        .map { index -> input.map { it[index] }.groupBy { it }.maxByOrNull { it.value.size }!!.key }
+        .joinToString("")
+        .toInt(2)
 
-        val count0 = counts['0'] ?: 0
-        val count1 = counts['1'] ?: 0
-        if (count1 > count0) '1' else '0'
-    }.joinToString("").toInt(2)
-
-    val epsilon = input[0].indices.map { index ->
-        val counts = input.map { it[index] }.groupBy { it }.mapValues { it.value.size }
-
-        val count0 = counts['0'] ?: 0
-        val count1 = counts['1'] ?: 0
-        if (count1 < count0) '1' else '0'
-    }.joinToString("").toInt(2)
+    val epsilon = input[0].indices
+        .map { index -> input.map { it[index] }.groupBy { it }.minByOrNull { it.value.size }!!.key }
+        .joinToString("")
+        .toInt(2)
 
     return gamma * epsilon
 }
 
 fun part2(): Int {
-    val oxygen = input[0].indices.fold(input) { acc, i ->
-        if (acc.size == 1) return@fold acc
+    val oxygen = input[0].indices
+        .fold(input) { acc, i ->
+            if (acc.size == 1) return@fold acc
 
-        val counts = acc.map { it[i] }.groupBy { it }.mapValues { it.value.size }
+            acc.map { it[i] }
+                .groupBy { it }
+                .toSortedMap(compareByDescending { it })
+                .maxByOrNull { it.value.size }!!.key
+                .let { value -> acc.filter { it[i] == value } }
+        }
+        .first()
+        .toInt(2)
 
-        val count0 = counts['0'] ?: 0
-        val count1 = counts['1'] ?: 0
+    val co2 = input[0].indices
+        .fold(input) { acc, i ->
+            if (acc.size == 1) return@fold acc
 
-        if (count1 >= count0) acc.filter { it[i] == '1' } else acc.filter { it[i] == '0' }
-    }.first().toInt(2)
-
-    val co2 = input[0].indices.fold(input) { acc, i ->
-        if (acc.size == 1) return@fold acc
-
-        val counts = acc.map { it[i] }.groupBy { it }.mapValues { it.value.size }
-
-        val count0 = counts['0'] ?: 0
-        val count1 = counts['1'] ?: 0
-
-        if (count1 < count0) acc.filter { it[i] == '1' } else acc.filter { it[i] == '0' }
-    }.first().toInt(2)
+            acc.map { it[i] }
+                .groupBy { it }
+                .toSortedMap()
+                .minByOrNull { it.value.size }!!.key
+                .let { value -> acc.filter { it[i] == value } }
+        }
+        .first()
+        .toInt(2)
 
     return oxygen * co2
 }

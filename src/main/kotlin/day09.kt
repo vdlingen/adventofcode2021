c0a7bf5afc1bc7f52fc6a3d6a4d9b43338eef704
Ronald van der Lingen <ronald@vdlingen.nl>
2021-12-09 07:52:44 +0100

    Day 9 - cleanup

--- src/main/kotlin/day09.kt
+++ src/main/kotlin/day09.kt
@@ -4,44 +4,41 @@ val input = util.readInput("day09.txt")
     .split("\n")
     .map { it.map { Integer.parseInt("$it") } }
 
-val width = input[0].size
-val height = input.size
+val mapWidth = input[0].size
+val mapHeight = input.size
 
-data class Point(val x: Int, val y: Int)
-
-val Point.value: Int get() = input[y][x]
+data class Point(val x: Int, val y: Int) {
+    val height: Int get() = input[y][x]
+}
 
 val points = buildList {
-    for (x in 0 until width)
-        for (y in 0 until height)
+    for (x in 0 until mapWidth)
+        for (y in 0 until mapHeight)
             add(Point(x, y))
 }
 
 fun Point.neighbors() = buildList {
     if (x > 0) add(Point(x - 1, y))
-    if (x < width - 1) add(Point(x + 1, y))
+    if (x < mapWidth - 1) add(Point(x + 1, y))
     if (y > 0) add(Point(x, y - 1))
-    if (y < height - 1) add(Point(x, y + 1))
+    if (y < mapHeight - 1) add(Point(x, y + 1))
 }
 
-val basins = points.filter { point -> point.neighbors().all { it.value > point.value } }
+val lowPoints = points.filter { point -> point.neighbors().all { it.height > point.height } }
+fun part1() = lowPoints.sumOf { it.height + 1 }
 
-fun part1() = basins.sumOf { it.value + 1 }
-
-fun part2() = basins.map { point ->
-    val basin = mutableSetOf(point)
-
-    fun expand(center: Point): Unit = center.neighbors().forEach {
-        if (it.value != 9 && it.value > center.value) {
-            basin += it
-            expand(it)
+val basins = lowPoints.map { point ->
+    buildSet {
+        fun expand(point: Point) {
+            add(point)
+            point.neighbors().forEach {
+                it.takeIf { it.height in (point.height + 1)..8 }?.let { expand(it) }
+            }
         }
+        expand(point)
     }
-
-    expand(point)
-
-    basin.size
-}.sortedDescending().subList(0, 3).reduce { a, b -> a * b }
+}
+fun part2() = basins.map { it.size }.sortedDescending().subList(0, 3).reduce { a, b -> a * b }
 
 fun main() {
     println("part 1 = ${part1()}")
